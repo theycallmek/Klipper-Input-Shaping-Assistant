c267950f4f495a68d037f94e2654a9af15158819
RoyalT <theycallmekenny@gmail.com>
2025-11-02 20:10:07 -0800

    Update copyright and refactor calibrate_shaper
    Updating to latest version from Klipper repo: https://raw.githubusercontent.com/Klipper3d/klipper/ba79d72fb47bc254f22382b355d8204eda9959c4/scripts/calibrate_shaper.py

--- calibrate_shaper.py
+++ calibrate_shaper.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # Shaper auto-calibration script
 #
-# Copyright (C) 2020  Dmitry Butyugin <dmbutyugin@google.com>
+# Copyright (C) 2020-2024  Dmitry Butyugin <dmbutyugin@google.com>
 # Copyright (C) 2020  Kevin O'Connor <kevin@koconnor.net>
 #
 # This file may be distributed under the terms of the GNU GPLv3 license.
@@ -9,16 +9,13 @@ from __future__ import print_function
 import importlib, optparse, os, sys
 from textwrap import wrap
 import numpy as np, matplotlib
-
-# sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)),
-#                              '..', 'klippy'))
+sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)),
+                             '..', 'klippy'))
 shaper_calibrate = importlib.import_module('.shaper_calibrate', 'extras')
 
-MAX_TITLE_LENGTH = 65
-
+MAX_TITLE_LENGTH=65
 
 def parse_log(logname):
-    print(f"Parsing log: {logname}")
     with open(logname) as f:
         for header in f:
             if not header.startswith('#'):
@@ -29,8 +26,8 @@ def parse_log(logname):
     # Parse power spectral density data
     data = np.loadtxt(logname, skiprows=1, comments='#', delimiter=',')
     calibration_data = shaper_calibrate.CalibrationData(
-        freq_bins=data[:, 0], psd_sum=data[:, 4],
-        psd_x=data[:, 1], psd_y=data[:, 2], psd_z=data[:, 3])
+            freq_bins=data[:,0], psd_sum=data[:,4],
+            psd_x=data[:,1], psd_y=data[:,2], psd_z=data[:,3])
     calibration_data.set_numpy(np)
     # If input shapers are present in the CSV file, the frequency
     # response is already normalized to input frequencies
@@ -38,13 +35,14 @@ def parse_log(logname):
         calibration_data.normalize_to_frequencies()
     return calibration_data
 
-
 ######################################################################
 # Shaper calibration
 ######################################################################
 
 # Find the best shaper parameters
-def calibrate_shaper(datas, csv_output, max_smoothing):
+def calibrate_shaper(datas, csv_output, *, shapers, damping_ratio, scv,
+                     shaper_freqs, max_smoothing, test_damping_ratios,
+                     max_freq):
     helper = shaper_calibrate.ShaperCalibrate(printer=None)
     if isinstance(datas[0], shaper_calibrate.CalibrationData):
         calibration_data = datas[0]
@@ -56,14 +54,22 @@ def calibrate_shaper(datas, csv_output, max_smoothing):
         for data in datas[1:]:
             calibration_data.add_data(helper.process_accelerometer_data(data))
         calibration_data.normalize_to_frequencies()
+
+
     shaper, all_shapers = helper.find_best_shaper(
-        calibration_data, max_smoothing, print)
+            calibration_data, shapers=shapers, damping_ratio=damping_ratio,
+            scv=scv, shaper_freqs=shaper_freqs, max_smoothing=max_smoothing,
+            test_damping_ratios=test_damping_ratios, max_freq=max_freq,
+            logger=print)
+    if not shaper:
+        print("No recommended shaper, possibly invalid value for --shapers=%s" %
+              (','.join(shapers)))
+        return None, None, None
     print("Recommended shaper is %s @ %.1f Hz" % (shaper.name, shaper.freq))
     if csv_output is not None:
         helper.save_calibration_data(
-            csv_output, calibration_data, all_shapers)
+                csv_output, calibration_data, all_shapers)
     return shaper.name, all_shapers, calibration_data
-
 
 ######################################################################
 # Plot frequency response and suggested input shapers
@@ -71,6 +77,9 @@ def calibrate_shaper(datas, csv_output, max_smoothing):
 
 def plot_freq_response(lognames, calibration_data, shapers,
                        selected_shaper, max_freq):
+    max_freq_bin = calibration_data.freq_bins.max()
+    if max_freq > max_freq_bin:
+        max_freq = max_freq_bin
     freqs = calibration_data.freq_bins
     psd = calibration_data.psd_sum[freqs <= max_freq]
     px = calibration_data.psd_x[freqs <= max_freq]
@@ -95,7 +104,7 @@ def plot_freq_response(lognames, calibration_data, shapers,
     ax.set_title("\n".join(wrap(title, MAX_TITLE_LENGTH)))
     ax.xaxis.set_minor_locator(matplotlib.ticker.MultipleLocator(5))
     ax.yaxis.set_minor_locator(matplotlib.ticker.AutoMinorLocator())
-    ax.ticklabel_format(axis='y', style='scientific', scilimits=(0, 0))
+    ax.ticklabel_format(axis='y', style='scientific', scilimits=(0,0))
     ax.grid(which='major', color='grey')
     ax.grid(which='minor', color='lightgrey')
 
@@ -104,9 +113,9 @@ def plot_freq_response(lognames, calibration_data, shapers,
     best_shaper_vals = None
     for shaper in shapers:
         label = "%s (%.1f Hz, vibr=%.1f%%, sm~=%.2f, accel<=%.f)" % (
-            shaper.name.upper(), shaper.freq,
-            shaper.vibrs * 100., shaper.smoothing,
-            round(shaper.max_accel / 100.) * 100.)
+                shaper.name.upper(), shaper.freq,
+                shaper.vibrs * 100., shaper.smoothing,
+                round(shaper.max_accel / 100.) * 100.)
         linestyle = 'dotted'
         if shaper.name == selected_shaper:
             linestyle = 'dashdot'
@@ -124,7 +133,6 @@ def plot_freq_response(lognames, calibration_data, shapers,
     fig.tight_layout()
     return fig
 
-
 ######################################################################
 # Startup
 ######################################################################
@@ -137,7 +145,6 @@ def setup_matplotlib(output_to_file):
     import matplotlib.pyplot, matplotlib.dates, matplotlib.font_manager
     import matplotlib.ticker
 
-
 def main():
     # Parse command-line arguments
     usage = "%prog [options] <logs>"
@@ -147,28 +154,94 @@ def main():
     opts.add_option("-c", "--csv", type="string", dest="csv",
                     default=None, help="filename of output csv file")
     opts.add_option("-f", "--max_freq", type="float", default=200.,
-                    help="maximum frequency to graph")
-    opts.add_option("-s", "--max_smoothing", type="float", default=None,
-                    help="maximum shaper smoothing to allow")
+                    help="maximum frequency to plot")
+    opts.add_option("-s", "--max_smoothing", type="float", dest="max_smoothing",
+                    default=None, help="maximum shaper smoothing to allow")
+    opts.add_option("--scv", "--square_corner_velocity", type="float",
+                    dest="scv", default=5., help="square corner velocity")
+    opts.add_option("--shaper_freq", type="string", dest="shaper_freq",
+                    default=None, help="shaper frequency(-ies) to test, " +
+                    "either a comma-separated list of floats, or a range in " +
+                    "the format [start]:end[:step]")
+    opts.add_option("--shapers", type="string", dest="shapers", default=None,
+                    help="a comma-separated list of shapers to test")
+    opts.add_option("--damping_ratio", type="float", dest="damping_ratio",
+                    default=None, help="shaper damping_ratio parameter")
+    opts.add_option("--test_damping_ratios", type="string",
+                    dest="test_damping_ratios", default=None,
+                    help="a comma-separated list of damping ratios to test " +
+                    "input shaper for")
     options, args = opts.parse_args()
     if len(args) < 1:
         opts.error("Incorrect number of arguments")
     if options.max_smoothing is not None and options.max_smoothing < 0.05:
         opts.error("Too small max_smoothing specified (must be at least 0.05)")
 
+    max_freq = options.max_freq
+    if options.shaper_freq is None:
+        shaper_freqs = []
+    elif options.shaper_freq.find(':') >= 0:
+        freq_start = None
+        freq_end = None
+        freq_step = None
+        try:
+            freqs_parsed = options.shaper_freq.partition(':')
+            if freqs_parsed[0]:
+                freq_start = float(freqs_parsed[0])
+            freqs_parsed = freqs_parsed[-1].partition(':')
+            freq_end = float(freqs_parsed[0])
+            if freq_start and freq_start > freq_end:
+                opts.error("Invalid --shaper_freq param: start range larger " +
+                           "than its end")
+            if freqs_parsed[-1].find(':') >= 0:
+                opts.error("Invalid --shaper_freq param format")
+            if freqs_parsed[-1]:
+                freq_step = float(freqs_parsed[-1])
+        except ValueError:
+            opts.error("--shaper_freq param does not specify correct range " +
+                       "in the format [start]:end[:step]")
+        shaper_freqs = (freq_start, freq_end, freq_step)
+        max_freq = max(max_freq, freq_end * 4./3.)
+    else:
+        try:
+            shaper_freqs = [float(s) for s in options.shaper_freq.split(',')]
+        except ValueError:
+            opts.error("invalid floating point value in --shaper_freq param")
+        max_freq = max(max_freq, max(shaper_freqs) * 4./3.)
+    if options.test_damping_ratios:
+        try:
+            test_damping_ratios = [float(s) for s in
+                                   options.test_damping_ratios.split(',')]
+        except ValueError:
+            opts.error("invalid floating point value in " +
+                       "--test_damping_ratios param")
+    else:
+        test_damping_ratios = None
+    if options.shapers is None:
+        shapers = None
+    else:
+        shapers = options.shapers.lower().split(',')
+
     # Parse data
     datas = [parse_log(fn) for fn in args]
 
     # Calibrate shaper and generate outputs
     selected_shaper, shapers, calibration_data = calibrate_shaper(
-        datas, options.csv, options.max_smoothing)
+            datas, options.csv, shapers=shapers,
+            damping_ratio=options.damping_ratio,
+            scv=options.scv, shaper_freqs=shaper_freqs,
+            max_smoothing=options.max_smoothing,
+            test_damping_ratios=test_damping_ratios,
+            max_freq=max_freq)
+    if selected_shaper is None:
+        return
 
     if not options.csv or options.output:
         # Draw graph
         setup_matplotlib(options.output is not None)
 
         fig = plot_freq_response(args, calibration_data, shapers,
-                                 selected_shaper, options.max_freq)
+                                 selected_shaper, max_freq)
 
         # Show graph
         if options.output is None:
@@ -176,7 +249,6 @@ def main():
         else:
             fig.set_size_inches(8, 6)
             fig.savefig(options.output)
-
 
 if __name__ == '__main__':
     main()
